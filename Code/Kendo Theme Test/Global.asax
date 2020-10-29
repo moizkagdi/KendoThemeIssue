7c5ec10ea1e44859884eaa0c8b6f1c80896600f1
Moiz Kagdi <moiz@qbt.com.au>
2020-10-29 12:52:34 +1100

    Project Commit -showing the border issue

---
+++ Code/Kendo Theme Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kendo_Theme_Test.MvcApplication" Language="C#" %>
---
+++ Code/Kendo Theme Test/Models/TestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kendo_Theme_Test.Models
+{
+    public class TestModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string PinCode { get; set; }
+
+        public TestModel()
+        {
+
+        }
+    }
+}
